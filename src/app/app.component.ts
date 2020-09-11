da4c2e709eb8cef8bf08ce68136a8bbaf62cd798
RohitvkUI <rohitvk2724@gmail.com>
2020-09-11 12:56:20 -0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component, ViewChild, ViewContainerRef, TemplateRef} from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, TemplateRef, OnInit} from '@angular/core';
 import sdk from '@stackblitz/sdk';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-    @ViewChild('rightDrawerContainer', {read: ViewContainerRef}) rightDrawerContainer:ViewContainerRef;
-    @ViewChild('miniCart', {read: TemplateRef}) miniCart: TemplateRef<any>;
-    @ViewChild('offerDetail', {read: TemplateRef}) offerDetail: TemplateRef<any>;
-    @ViewChild('couponList', {read: TemplateRef}) couponList: TemplateRef<any>;
+export class AppComponent implements OnInit {
+  @ViewChild('rightDrawerContainer', {read: ViewContainerRef}) rightDrawerContainer:ViewContainerRef;
+  @ViewChild('miniCart', {read: TemplateRef}) miniCart: TemplateRef<any>;
+  @ViewChild('offerDetail', {read: TemplateRef}) offerDetail: TemplateRef<any>;
+  @ViewChild('couponList', {read: TemplateRef}) couponList: TemplateRef<any>;
+
+
+  public ngOnInit(){
+    this.rightDrawerContainer.createEmbeddedView(this.miniCart);
+  }
+
+  public changeComponent(component){
+    switch(component){
+      case 'offer-detail':
+      this.rightDrawerContainer.detach();
+      this.rightDrawerContainer.createEmbeddedView(this.miniCart);
+      break;
+      case 'mini-cart':
+      this.rightDrawerContainer.detach();
+      this.rightDrawerContainer.createEmbeddedView(this.offerDetail);
+      break;
+      case 'coupon-list':
+      this.rightDrawerContainer.detach();
+      this.rightDrawerContainer.createEmbeddedView(this.couponList);
+      break;
+    }
+  }
+
+
+
 }
